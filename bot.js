8dff8d8ce4322dd7b41a2875113f6b772e289163
snowtk <75138288+snowtk@users.noreply.github.com>
2022-08-07 21:21:37 +0100

    publish

---
+++ bot.js
@@ -0,0 +1,19 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const keepAlive = require("./server")
+const token = process.env['token']
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+  console.log(interaction);
+  if (!interaction.isChatInputCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+keepAlive();
+client.login(token);
---
+++ index.js
@@ -0,0 +1,24 @@
+const { REST, Routes } = require('discord.js');
+const CLIENT_ID = process.env['client_id']
+const GUILD_ID = process.env['guild_id']
+const token = process.env['token']
+const commands = [
+  {
+    name: 'ping',
+    description: 'Replies with Pong!',
+  },
+];
+
+const rest = new REST({ version: '10' }).setToken(token);
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
